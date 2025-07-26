7631ec98ce140c995d96e6b5ea812a0dabe07b39
Anil Charles <anilcharles96@gmail.com>
2025-07-26 16:56:27 +0200

    add script

---
+++ README.md
@@ -0,0 +1,16 @@
+# Script that parse movie title
+
+### Prerequisites
+```
+pip install -r requirements.txt
+```
+
+### Run
+```
+python main.py <title>
+
+eg: 
+python main.py star
+python main.py spiderman
+python main.py home
+```
---
+++ main.py
@@ -0,0 +1,84 @@
+import requests
+import json 
+import argparse
+
+
+def get_data_from_page(title):
+    """
+    Fetches movie data from the API based on the provided title.
+
+    Args:
+        title (str): The title of the movie to search for.
+    Returns:
+        dict: A dictionary containing movie data.
+    Raises:
+        Exception: If there is an error fetching data from the API.
+    Example:
+        get_data_from_page("spiderman")
+
+    """
+    # Return if title is empty
+    if not title:
+        print("Title cannot be empty.")
+        return
+
+    url = f"https://jsonmock.hackerrank.com/api/movies/search/?Title={title}"
+    # Get request to the API
+    try:
+        response = requests.get(url)
+        movie_data = json.loads(response.text)
+    except Exception as e:
+        print(f"Error fetching data: {e}")
+        
+    return movie_data
+
+def getMovieTitles(title):
+    """
+    Fetches unique movie titles from the API based on the provided title.
+
+    Args:
+        title (str): The title of the movie to search for.
+    Returns:
+        list: A list of unique movie titles sorted alphabetically.
+    """
+    movie_data = get_data_from_page(title)    
+    data = movie_data["data"]
+    total_pages =  movie_data["total_pages"]
+    total = movie_data["total"]
+
+    for page in range(2, total_pages + 1):
+        page_data = get_data_from_page(title + f"&page={page}")
+        data.extend(page_data["data"])
+    
+    # Check if data is empty
+    if not data:
+        print("No movies found.")
+        return
+
+    # Extract movie titles from the data
+    titles = [movie["Title"] for movie in data]
+
+    # Remove duplicates, Question asks for unique titles
+    titles = list(set(titles))
+
+    # Sort the titles alphabetically
+    titles.sort()
+
+    return titles
+    
+
+def main():
+    parser = argparse.ArgumentParser(description="Fetch movie titles from Hackerrank API.")
+    parser.add_argument("title", type=str, help="Title of the movie to search for")
+    args = parser.parse_args()
+
+    titles = getMovieTitles(args.title)
+    if titles:
+        print("Movie Titles:")
+        for title in titles:
+            print(title)
+    else:
+        print("No titles to display.")
+
+if __name__ == "__main__":
+    main()
